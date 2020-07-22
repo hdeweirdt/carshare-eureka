f897f711861cc76de308e2feb90494bd1db71d50
Harm De Weirdt <harm.deweirdt@mailfence.com>
2020-07-22 08:09:38 +0200

    More specific csrf configuration

--- src/main/java/be/harm/carshare/eureka/config/SecurityConfig.java
+++ src/main/java/be/harm/carshare/eureka/config/SecurityConfig.java
@@ -9,7 +9,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf().ignoringAntMatchers("/eureka/**")
+                .and()
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
